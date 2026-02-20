d9b07b12133074a745b7fd15a40e5f8be606a584
Ivaneta Ivanova <ivanetaivanova0908@gmail.com>
2026-02-20 10:26:46 +0200

    Fix Spring Boot initialization
    Introduce a new kanban-api Spring Boot module scaffold. Adds Maven wrapper (mvnw, mvnw.cmd, .mvn/wrapper), pom.xml, application.yaml, main application class and a basic test, plus .gitignore and .gitattributes for repo hygiene. Also simplify .idea/vcs.xml VCS mapping entry.

---
+++ kanban-api/src/test/java/com/master/kanban_api/KanbanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.kanban_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
