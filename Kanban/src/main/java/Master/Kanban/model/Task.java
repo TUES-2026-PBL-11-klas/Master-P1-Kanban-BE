e81fbb06a1ec1a5d144b3f042e5f2b916416f7fd
bananateo <teodorkamchev@gmail.com>
2026-02-23 17:32:08 +0200

    Improved task entity
    Made it compatible with the database

--- Kanban/src/main/java/Master/Kanban/model/Task.java
+++ Kanban/src/main/java/Master/Kanban/model/Task.java
@@ -1,7 +1,9 @@
 package Master.Kanban.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,14 +12,23 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "tasks")
 public class Task {
     @Id
+    @Column(name = "Position", nullable = false)
     private int index;
+    @Column(name = "Token", nullable = false)
     private long UsrAuthT;
+    @Column(name = "Deleted", nullable = false)
     private boolean deleted;
+    @Column(name = "Name")
     private String title;
+    @Column(name = "Description")
     private String desc;
+    @Column(name = "Priority", nullable = false)
     private int priority;
+    @Column(name = "State", nullable = false)
     private int state;
+    @Column(name = "Time", nullable = false)
     private String tszImplement; //not implemented
 }
