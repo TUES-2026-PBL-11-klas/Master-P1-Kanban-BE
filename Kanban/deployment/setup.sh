a2d92f9d067917f5e9b48bbf7442edc6584887d7
AlexK-TUES <124814427+AlexK-TUES@users.noreply.github.com>
2026-02-25 16:04:16 +0200

    These were stashed

--- Kanban/deployment/setup.sh
+++ Kanban/deployment/setup.sh
@@ -1,15 +1,30 @@
+set -e
 
-echo "Step 1: Checking for .env file..."
+CURRENT_USER=$(whoami)
+CURRENT_DIR=$(pwd)
+
+echo "$CURRENT_USER to be used for configuration of systemd service in $CURRENT_DIR. Deploying service."
+
+sed -e "s|{{USER}}|$CURRENT_USER|g" \
+    -e "s|{{WORKDIR}}|$CURRENT_DIR|g" \
+    kanban.service.template > kanban.service
+
+sudo mv kanban.service /etc/systemd/system/kanban.service
+sudo systemctl daemon-reload
+sudo systemctl enable kanban.service
+sudo systemctl start kanban.service --now
+
+echo "Checking for .env file in $CURRENT_DIR"
 if [ ! -f .env ]; then
     echo "Error: .env file not found! Copy .env.example and fill it in."
     exit 1
 fi
 
-echo "Step 2: Starting Database..."
+echo "Starting Database container"
 docker-compose -f docker-compose.yml up -d
 
-echo "Step 3: Building Application..."
+echo "Building Application"
 cd ..
 ./mvnw clean package -DskipTests
 
-echo "Step 4: Deployment complete."
+echo "Deployment complete."
